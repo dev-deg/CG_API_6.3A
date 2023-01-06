e93eab2cf35ff6f8df1ff183f5ad9b7408a16412
deg <david.deguara@mcast.edu.mt>
2023-01-06 09:11:43 +0100

    api comments

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 
+//Setting up an instance of express server
 const app = express();
+
+//Keeping track on the number of web requests
 var reqCount = 0;
+
+//Generating a random integer
 const getRandomInt = (max) => Math.floor(Math.random() * max);
 
+//localhost:4000
 app.get("/", (req, res) => res.send("Hello World"));
 
+//localhost:4000/random
 app.post("/random", (req, res) => {
   reqCount++;
   var max = 100;
@@ -15,4 +22,5 @@ app.post("/random", (req, res) => {
   return res.send(JSON.stringify({ request: reqCount, random: getRandomInt(max).toString() }));
 });
 
+//Starts the server
 app.listen(4000, () => console.log(`API listening in port 4000`));
