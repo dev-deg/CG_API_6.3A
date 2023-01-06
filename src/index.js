2e063161df49af5aca6fb6ff0b0c64840fc4e23f
deg <david.deguara@mcast.edu.mt>
2023-01-06 09:01:18 +0100

    Accepting parameters

--- src/index.js
+++ src/index.js
@@ -11,7 +11,11 @@ app.get("/", (req, res) => {
 });
 
 app.get("/random", (req, res) => {
-  return res.send(getRandomInt(100).toString());
+  var max = 100;
+  if (req.query.max) {
+    max = parseInt(req.query.max);
+  }
+  return res.send(getRandomInt(max).toString());
 });
 
 app.listen(4000, () => console.log(`API listening in port 4000`));
